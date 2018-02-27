0e301a351d188c88117d00fcf5d5d5f57f64dca0
JonathanQuang <jquang@stuy.edu>
2018-02-27 00:40:10 -0500

    why no work?

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-import pymongo
+from flask import Flask, render_template, session, flash, request
+import pymongo, requests
 
 
 app = Flask(__name__)
@@ -8,15 +8,57 @@ connection=pymongo.MongoClient("homer.stuy.edu")
 db = connection['usStats']
 collection = db['usStatsCollection']
 
-droppedData = collection.drop()
+collection.drop()
 
-print droppedData
+file = open("populationUSA.json","r")
+data = file.read()
 
+#print(data)
+
+#parse the data
+
+#first remove the starting and ending [{ and }]
+data = data[2:len(data)-2]
+
+#split along }, {
+data = data.split("}, {")
+
+
+
+for index in data:
+    dataDict = {}
+    rowSplit = index.split(', ')
+    for dataPair in rowSplit:
+		colonIndex = dataPair.find(':')
+		theKeyNoQuotes = dataPair[1:colonIndex-1]
+		theValue = dataPair[colonIndex+2:len(dataPair)]
+		theValue = theValue.strip('"')
+		try:
+			theValue = int(theValue)
+		except:
+			print 'noIntConvert'
+		dataDict[theKeyNoQuotes]=theValue
+    print dataDict
+    collection.insert_one(dataDict)
 
 @app.route('/')
 def root():
-	return('run')
+	return render_template("base.html")
+	
+@app.route('/response',methods=["GET","POST"])
+def response():
+	lowerBound = int(request.form['lowerRange'])
+	upperBound = int(request.form['upperRange'])
+	cursor = collection.find({"males":{"$gt":lowerBound,"$lt":upperBound}})
+	flashList = list()
+	print str(lowerBound) + " | " + str(upperBound)
+	print cursor
+	for entry in cursor:
+		print entry
+		print entry['age']
+		flash(entry['age'])
+	return render_template("base.html")
 
 if __name__ == "__main__":
-    echo_app.debug = True
-    echo_app.run()	
+    app.debug = True
+    app.run()	
